fd5624c7e4ae4db037f7fd0415c278a33b2e09a7
wanted <wanted.swift57@gmail.com>
2024-02-26 00:11:26 +0000

    test

--- sage_stream/utils/stream_services.py
+++ sage_stream/utils/stream_services.py
@@ -40,8 +40,9 @@ def get_streaming_response(path, range_header, range_re, max_load_volume):
         first_byte, last_byte = range_match.groups()
         first_byte = get_first_byte(first_byte)
 
-        last_byte = get_last_byte(first_byte, max_load_volume)
+        #last_byte = get_last_byte(first_byte, max_load_volume)
 
+        last_byte = int(last_byte) or last_byte else or - 1
 
         if last_byte >= size:
             last_byte = size - 1
